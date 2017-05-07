abc4dcb9f8ed4eca2cf82fe028ebb947b54e2fe6
David Hervas <bioestadistica@iislafe.es>
2017-05-07 16:27:29 +0200

    Reduced running time for examples

--- R/sNPLS_fit.R
+++ R/sNPLS_fit.R
@@ -181,7 +181,7 @@ unfold3w <- function(x) {
 #' Y_npls<-matrix(2+0.4*X_npls[,5,1]+0.7*X_npls[,10,1]-0.9*X_npls[,15,1]+
 #' 0.6*X_npls[,20,1]- 0.5*X_npls[,25,1]+rnorm(50), ncol=1)
 #'
-#' cv1<- cv_snpls(X_npls, Y_npls, ncomp=1:2, keepJ = 1:5, keepK = 1:3, parallel = FALSE)
+#' cv1<- cv_snpls(X_npls, Y_npls, ncomp=1:2, keepJ = 1:3, keepK = 1:2, parallel = FALSE)
 #' @export
 cv_snpls <- function(X_npls, Y_npls, ncomp = 1:3, keepJ = 1:ncol(X_npls), keepK = 1:dim(X_npls)[3],
     nfold = 10, parallel = TRUE, free_cores = 2) {
