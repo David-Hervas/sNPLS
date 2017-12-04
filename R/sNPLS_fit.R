b85cbcba6c49485383bfc05d15944757a1db1d17
David Hervas <bioestadistica@iislafe.es>
2017-12-04 14:27:17 +0100

    Fixed missing scale and centering attributes in sNPLS objects

--- R/sNPLS_fit.R
+++ R/sNPLS_fit.R
@@ -40,7 +40,10 @@ sNPLS <- function(XN, Y, ncomp = 2, conver = 1e-16, max.iteration = 10000,
         x3d.names <- dimnames(XN)[[3]] else x3d.names <- paste("Z.", 1:dim(XN)[3], sep = "")
     if (!is.null(colnames(Y)))
         yvar.names <- colnames(Y) else yvar.names <- paste("Y.", 1:dim(Y)[2], sep = "")
-
+    if(!scale.X) scale.X <- 1
+    if(!scale.Y) scale.Y <- 1
+    if(!center.X) center.X <- 0
+    if(!center.Y) center.Y <- 0
     # Matrices initialization
     Tm <- U <- Q <- WsupraJ <- WsupraK <- X <- P <- NULL
     Yorig <- Y
